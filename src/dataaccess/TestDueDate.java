ee018e7c68c4c553c8f04ad07c33296bc6dd5152
vien <viennv@newbees.jp>
2023-12-08 21:11:45 -0600

    add test function for overdue date book

---
+++ src/dataaccess/TestDueDate.java
@@ -0,0 +1,30 @@
+package dataaccess;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import business.LibrarySystemException;
+import business.LibraryMember;
+import business.CheckoutRecordEntry;
+
+public class TestDueDate {
+
+	public static void main(String[] args) {
+		ArrayList<String> listMemberId = new ArrayList<>();
+		listMemberId.add("1004");
+		try {
+			for(String str : listMemberId) {
+				DataAccess dc = new DataAccessFacade();
+				LibraryMember mem = dc.searchMember(str);
+				for( CheckoutRecordEntry entry : mem.getRecord().getRecords()) {
+					LocalDate date = LocalDate.now();
+					entry.setDueDate(date.minusDays(2));
+				}
+				dc.saveNewMember(mem);
+				System.out.println("finished update member : " + mem.getFirstName());
+			}
+			
+		} catch(LibrarySystemException e) {
+			System.out.println("error : " + e.getMessage());
+		}
+		
+	}
+}
